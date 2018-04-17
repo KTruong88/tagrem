a436f89b028ce1fdeeb1a6aed1aef8f3443061c5
Kevin Truong <kevintru0ng@yahoo.com>
2018-04-17 15:19:55 -0700

    product1 modal complete

--- js/script.js
+++ js/script.js
@@ -6,3 +6,9 @@ $(document).ready(function(){
         $("#dropdown-1").hide();
     });
 });
+
+$(document).ready(function() {
+    $(".product-title, .starting-at, .product-text1, .product-img-1, .learn-more1").click(function(event){
+        $('html, body').animate({scrollTop: '+=650px'}, 700);
+    });
+});
